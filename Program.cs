be7fe8cb0f1d03587d9fd040887aa5b904dbb11a
Hemachandran Perumal <hemachandranpm@gmail.com>
2024-12-25 09:32:56 +0530

    Reverse string

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string original = "Hello, World!";
+        string reversed = ReverseString(original);
+        Console.WriteLine(reversed);
+    }
+
+    static string ReverseString(string input)
+    {
+        char[] charArray = new char[input.Length];
+        int forwardIndex = 0;
+        int backwardIndex = input.Length - 1;
+
+        while (backwardIndex >= 0)
+        {
+            charArray[forwardIndex] = input[backwardIndex];
+            forwardIndex++;
+            backwardIndex--;
+        }
+
+        return new string(charArray);
+    }
+}
